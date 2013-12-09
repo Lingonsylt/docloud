17c27a09870edcbf4668c74f81ffd6561c25c5b0
Anton <anton@naetet.se>
2013-12-09 12:34:01 +0100

    Fixed typo

--- web/searcher/urls.py
+++ web/searcher/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from searcher import views
 urlpatterns = patterns('',
-                       url(r'^ladda-ner/(?P<hash>\w+)/', views.download, name ="download"),
+                       url(r'^ladda-ner/(?P<hash_>\w+)/', views.download, name ="download"),
                        url(r'', views.search, name ="search"),
                        )
