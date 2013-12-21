fcd64785451b2966587240329d009caf8088e3bd
Anton <anton@naetet.se>
2013-12-21 13:29:08 +0100

    Updated install.py with better error messages and better default options

--- client/install.py
+++ client/install.py
@@ -10,12 +10,12 @@ if sys.version_info < (3, 3, 0):
 
 script_path = os.path.dirname(os.path.abspath(__file__))
 
-def _get_pkg_path():
+def _get_pkg_path(default=None):
     args = sys.argv[1:]
     if "--package-path" in args:
         pkg_path = args[args.index("--package-path") +1]
     else:
-        pkg_path = join(script_path, "pkg")
+        pkg_path = join(script_path, default if default is not None else "pkg")
     return pkg_path
 
 def package(bits="x64"):
@@ -50,23 +50,42 @@ def install(pkg_path):
     reinstall = "--reinstall" in args or "-r" in args
 
     if reinstall:
-        uninstall(install_path)
+        if not uninstall(install_path):
+            print("Uninstall failed! Not running re-installation!")
     print("Installing package from: %s" % pkg_path)
     previous_install_path = _get_registry_install_path()
     if previous_install_path is not None:
-        print("Docloud is already installed!")
+        print("Docloud is already installed! No install needed (use -r to reinstall)")
+        return
     if os.path.exists(install_path):
-        print("Path already exists: %s! (use -r to reinstall)" % install_path)
+        print("Path already exists: %s! No install run (use -r to reinstall)" % install_path)
         return
 
     _set_registry_install_path(install_path)
     print("Installing in: %s" % install_path)
-    copytree(pkg_path, install_path)
+    try:
+        copytree(pkg_path, install_path)
+    except OSError as e:
+        print("Error copying files: %s" % e)
+        print("Install stopped")
+        return
     db_path = join(install_path, "db.sqlite")
     schema_path = join(pkg_path, "schema.sql") if os.path.exists(join(pkg_path, "schema.sql")) else join(pkg_path, "shared", "schema.sql")
-    conn = sqlite3.connect(db_path)
-    with open(schema_path) as schema_f:
-        conn.executescript(schema_f.read())
+
+    try:
+        conn = sqlite3.connect(db_path)
+    except Exception as e:
+        print("Could not connect to dababase: %s" % e)
+        print("Install stopped")
+        return
+
+    try:
+        with open(schema_path) as schema_f:
+            conn.executescript(schema_f.read())
+    except Exception as e:
+        print("Could not create schema in db: %s" % e)
+        print("Install stopped")
+        return
 
     print("Loading dll: %s" % join(install_path, "docloudext.dll"))
     os.system("regsvr32 %s" % join(install_path, "docloudext.dll"))
@@ -94,6 +113,7 @@ def _delete_registry_keys():
     winreg.DeleteKey(docloud_key, "")
 
 def uninstall(new_path=None):
+    success = True
     print("Uninstalling...")
     install_path = _get_registry_install_path()
     if install_path is not None:
@@ -104,8 +124,13 @@ def uninstall(new_path=None):
             os.system("regsvr32 /u %s" % docloudext_dll)
         os.system("taskkill /f /im explorer.exe")
         time.sleep(0.5)
+
         if os.path.exists(install_path) and os.path.isdir(install_path):
-            rmtree(install_path)
+            try:
+                rmtree(install_path)
+            except OSError as e:
+                print("Error removing files: %s" % e)
+                success = False
         os.system("explorer")
         time.sleep(0.5)
     else:
@@ -115,7 +140,11 @@ def uninstall(new_path=None):
             rmtree(new_path)
         except FileNotFoundError:
             pass
+        except OSError as e:
+            print("Error removing files: %s" % e)
+            success = False
     _delete_registry_keys()
+    return success
 
 args = sys.argv[1:]
 if "uninstall" in args:
@@ -125,6 +154,6 @@ if "package" in args:
     if "install" in args:
         install(pkg_path)
 elif "install" in args:
-    install(_get_pkg_path())
+    install(_get_pkg_path("."))
 elif "uninstall" not in args:
     print("Options are: package, install, uninstall, --killexplorer (-k), --reinstall (-r) --package-path")
