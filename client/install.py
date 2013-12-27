8957df7d6c6790c9ba4aa856632b0db6d206f355
xyzzy <xyzzy@kudzu.se>
2013-12-27 23:51:51 +0100

    Add settings-application to install-script, fix global build

--- client/install.py
+++ client/install.py
@@ -28,6 +28,7 @@ def package(bits="x64"):
     print("Creating package at: %s" % pkg_path)
     service_path = join(script_path, "service")
     shared_path = join(script_path, "shared")
+    settings_path = join(script_path, "settings")
     docloudext_path = join(script_path, "docloudext")
 
     try:
@@ -40,13 +41,19 @@ def package(bits="x64"):
     copy(join(shared_path, "sqlite3", "sqlite3.dll"), tmppkg_path)
     copy(join(shared_path, "schema.sql"), tmppkg_path)
 
-    dlls = join(shared_path, bits)
+    if bits == "x64":
+        dlls = join(shared_path, "libwin32", "gtk3")
+    else:
+        dlls = join(shared_path, "libwin64", "gtk3")
+
     if os.path.exists(dlls) and os.path.isdir(dlls):
         for file in os.listdir(dlls):
-            copy(join(shared_path, bits, file), tmppkg_path)
+            if os.path.isfile(join(dlls,file)):
+                copy(join(dlls, file), tmppkg_path)
 
     copy(join(docloudext_path, "docloudext.dll"), tmppkg_path)
     copy(join(service_path, "docloud-svc.exe"), tmppkg_path)
+    copy(join(settings_path, "docloud-settings.exe"), tmppkg_path)
     _zipdir(tmppkg_path, os.path.join(pkg_path, "pkg.zip"))
     rmtree(tmppkg_path)
     print("Package created at: %s" % pkg_path)
